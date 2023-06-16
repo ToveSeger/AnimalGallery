98974febabc0bb74937a650b5d2011e051ea2e86
ToveSeger <tove.seger@epicalgroup.com>
2023-06-16 13:45:21 +0200

    route change in drawer

--- src/components/drawer/Drawer.tsx
+++ src/components/drawer/Drawer.tsx
@@ -10,8 +10,8 @@ export const Drawer = (props:IDrawer) => {
             <RxCross2/>
         </button>
         <a href="/">Home</a>
-        <a href="/contact">Exotic animals</a>
-        <a href="/contact">Scandinavian animals</a>
+        <a href="/exotic">Exotic animals</a>
+        <a href="/scandinavian">Scandinavian animals</a>
         <a href="/about">About us</a>
         <a href="/contact">Contact</a>
         <section className={styles.snailEyes}>
